2930677be617042b0f21209f0f269532442acf0a
gdare <gdare@caltech.edu>
2025-11-03 17:19:35 -0500

    Extended the maximum number of radial points

--- src/NumericalAlgorithms/Spectral/MaximumNumberOfPoints.hpp
+++ src/NumericalAlgorithms/Spectral/MaximumNumberOfPoints.hpp
@@ -22,5 +22,5 @@ namespace Spectral {
  */
 template <Basis basis>
 constexpr size_t maximum_number_of_points =
-    basis == Basis::FiniteDifference ? 40 : 20;
+    basis == Basis::FiniteDifference ? 40 : 40;
 }  // namespace Spectral
